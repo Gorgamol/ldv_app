b9f6270b13bc2bce3804a9c6060c608b6b8e8f20
Gorgamol <Gorgamol@users.noreply.github.com>
2025-08-29 22:32:14 +0200

    Basic app setup

---
+++ lib/ldv_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LdvApp extends StatelessWidget {
+  const LdvApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LDV App',
+      theme: ThemeData(),
+      home: const Placeholder(),
+    );
+  }
+}
