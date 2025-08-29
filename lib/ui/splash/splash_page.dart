2ca27aa05065b51a3384e6b3902086a1e9d4526d
Gorgamol <Gorgamol@users.noreply.github.com>
2025-08-29 23:29:09 +0200

    SplashPage setup for adaptive layout

--- lib/ui/splash/splash_page.dart
+++ lib/ui/splash/splash_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:ldv_app/ui/splash/splash_page_desktop.dart';
+import 'package:ldv_app/ui/splash/splash_page_mobile.dart';
+import 'package:ldv_app/ui/widgets/adaptive_layout.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const AdaptiveLayout(
+      mobile: SplashPageMobile(),
+      desktop: SplashPageDesktop(),
+    );
   }
 }
