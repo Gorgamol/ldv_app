3b2e421e275582c82d9a87116031c5605e2b83ab
Gorgamol <Gorgamol@users.noreply.github.com>
2025-08-30 00:03:45 +0200

    Added app theme colors

---
+++ lib/core/theme/ldv_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class LdvColors {
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const bitterSweet = Color(0xFFFF7474);
+}
